169aba1e077a48a92adfd7b9bb822da62cb893c7
Soveu <marx.tomasz@gmail.com>
2025-12-09 19:22:19 +0100

    glfw window

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    use glfw::fail_on_errors;
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
+    glfw.window_hint(glfw::WindowHint::Resizable(false));
+
+    let (window, _events) = glfw.create_window(800, 600, "Vulkant", glfw::WindowMode::Windowed).unwrap();
+    while !window.should_close() {
+        glfw.poll_events();
+    }
 }
